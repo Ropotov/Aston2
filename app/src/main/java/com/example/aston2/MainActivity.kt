c9cad1a19cb30c16d8feab29f3fe2062784da081
Ropotov Nikita <ropotov.dev@gmail.com>
2023-03-23 18:33:56 +0400

    initial commit

--- app/src/main/java/com/example/aston2/MainActivity.kt
+++ app/src/main/java/com/example/aston2/MainActivity.kt
@@ -1,33 +1,74 @@
 package com.example.aston2
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
 import com.example.aston2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        savedInstanceState?.let {
-            count = it.getInt("count")
+        binding.openWebSite.setOnClickListener {
+            openWebSite()
         }
 
-        binding.textView.text = count.toString()
-        binding.count.setOnClickListener {
-            count++
-            binding.textView.text = count.toString()
+        binding.openLocation.setOnClickListener {
+            openLocation()
         }
 
+        binding.shareText.setOnClickListener {
+            shareText()
+        }
+
+        binding.takePicture.setOnClickListener {
+            takeAPictures()
+        }
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("count", count)
+    private fun openWebSite() {
+        val uri = Uri.parse(binding.webSite.text.toString())
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        try {
+            startActivity(intent)
+        } catch (e: Exception) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun openLocation() {
+        val uri = Uri.parse("geo:0,0")
+            .buildUpon().appendQueryParameter("q", binding.location.text.toString()).build()
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        try {
+            startActivity(intent)
+        } catch (e: Exception) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun shareText(){
+        ShareCompat.IntentBuilder(this)
+            .setType("text/plain")
+            .setText(binding.text.text.toString())
+            .startChooser()
+    }
+
+    private fun takeAPictures(){
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivity(intent)
+        } catch (e: Exception) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
